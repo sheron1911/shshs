52ca6c8d8bd4226effaad93eb6706cb910b5462a
sheron1911 <115542580+sheron1911@users.noreply.github.com>
2024-10-22 23:17:56 +0800

    check

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 ?><!DOCTYPE html>
 <html>
   <head>
-    <title>Welcome to AWS Technical Essentials v4.1</title>
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>個人網站</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
   </head>
@@ -30,7 +32,32 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
     </div>
     </div>
     </div>
+    <section id="about">
+        <h2>個人簡介</h2>
+        <p>資工三甲11111143許立倫</p>
+    </section>
 
+    <section id="contact">
+        <h2>聯絡方式</h2>
+        <p>Email: a0960970426@gmail.com</p>
+        <p>電話: 0960970426</p>
+    </section>
+
+    <section id="portfolio">
+        <h2>展示作品</h2>
+        <h3>影像</h3>
+        <img src="1111111.jpg"width="300">
+
+        <h3>影片</h3>
+        <video controls width="300">
+            <source src="123.mp4" type="video/mp4">
+        </video>
+
+        <h3>聲音</h3>
+        <audio controls>
+            <source src="12.mp3" type="audio/mpeg">
+        </audio>
+    </section>
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/scripts.js"></script>
